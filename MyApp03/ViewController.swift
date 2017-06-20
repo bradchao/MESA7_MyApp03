0507c19ae7ec4e895fa1264024bef2f666939c3f
iii <iii@userteki-MacBook.local>
2017-06-20 14:03:45 +0800

    v5

--- MyApp03/ViewController.swift
+++ MyApp03/ViewController.swift
@@ -15,22 +15,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var textHistory: UITextView!
     
     private var stringAnswer:String?
+    private var counter = 0
     
     @IBAction func clickGuess(_ sender: Any) {
+        counter += 1
         let stringInput = textInput.text!
         
         let stringResult = BradAPI.checkAB(answer: stringAnswer!,
                                            guess:stringInput)
         labelResult.text = stringResult
         
-        textHistory.text.append("\(stringInput) => \(stringResult)\n")
+        textHistory.text.append("\(counter). \(stringInput) => \(stringResult)\n")
+        textInput.text = ""
+        
+        if stringResult == "3A0B" {
+            showWinnerDialog()
+        }else if counter == 10 {
+            showLoserDialog()
+        }
+        
+    }
+
+    func showWinnerDialog() {
+        let alert:UIAlertController = UIAlertController(title: "遊戲結果", message: "恭喜老爺, 賀喜夫人", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {
+            (action: UIAlertAction) -> Void in
+            print("OK")
+        })
+        
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func showLoserDialog() {
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        stringAnswer = BradAPI.createAnswer()
+        stringAnswer = BradAPI.createAnswer(3)
         //print(stringAnswer!)
         
     }
